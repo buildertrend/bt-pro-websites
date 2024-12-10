2cc68e6547ccf7017dec496f5502d2a15680a522
John Stuifbergen <johnstuif@gmail.com>
2024-12-10 11:48:10 -0600

    Adding auto updates

--- bt-pro-websites.php
+++ bt-pro-websites.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Buildertrend Pro Websites
 Description: Our required plugin for sharing common functionality across our client sites. 
-Version: 1.0.1.1
+Version: 1.0.2
 Author: John Stuifbergen
 License: GPLv2 or later
 */
@@ -15,3 +15,12 @@ if (!defined('ABSPATH')) {
 // Include the settings page code
 require_once plugin_dir_path(__FILE__) . 'includes/admin-page.php';
 
+
+add_filter('auto_update_plugin', 'bt_force_auto_update', 10, 2);
+
+function bt_force_auto_update($update, $item) {
+    if (isset($item->slug) && $item->slug === 'buildertrend-pro-websites') {
+        return true;  // Enable auto-updates for this plugin
+    }
+    return $update;
+}
