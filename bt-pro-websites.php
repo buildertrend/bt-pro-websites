ff37dba9f23bdca60a29cf25774e0d145a14e128
John Stuifbergen <johnstuif@gmail.com>
2024-12-10 10:49:55 -0600

    First commit

---
+++ bt-pro-websites.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: Buildertrend Pro Websites
+Description: Our required plugin for sharing common functionality across our client sites. 
+Version: 1.0
+Author: John Stuifbergen
+License: GPLv2 or later
+*/
+
+// Prevent direct access
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+// Core Plugin Functionality Here
