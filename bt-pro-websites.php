cb96ac797115b6c7669e1fe1d6255559aae0091f
John Stuifbergen <johnstuif@gmail.com>
2025-04-08 14:18:05 -0500

    Added dashboard widget

--- bt-pro-websites.php
+++ bt-pro-websites.php
@@ -34,3 +34,15 @@ $updateChecker = PucFactory::buildUpdateChecker(
 // Optional if you’re using a branch other than “master” (e.g., main)
 $updateChecker->setBranch('main');
 
+
+// Dashboard Widget Content
+function bt_pro_websites_dashboard_widget_content()
+{
+    // You can add any content or settings before the iframe
+    echo '<div class="bt-dashboard-iframe-container" style="position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%;">';
+    echo '<iframe src="https://www.wrike.com/form/eyJhY2NvdW50SWQiOjI1NzEyNTEsInRhc2tGb3JtSWQiOjcwMzkxNX0JNDgxMjA5OTQ0MjUxNAkwMGZhMDZjZjk5YjBiNDRkY2ZjNWUwYzJiM2M0OTJmZTgyMTJhNmJlNzJhMWM1MjI1N2Y1NDVmOTUwYWRlZWJi" width="100%" height="1400" frameborder="0"><span data-mce-type="bookmark" style="display: inline-block; width: 0px; overflow: hidden; line-height: 0;" class="mce_SELRES_start">﻿</span></iframe>';
+    echo '</div>';
+
+    // Optional: Add some content after the iframe
+    echo '<p><small>Powered by Buildertrend Pro Websites</small></p>';
+}
