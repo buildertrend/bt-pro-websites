4cbe3f510dba69c832ffd1dd59ecf823802142a0
John Stuifbergen <johnstuif@gmail.com>
2025-04-08 12:25:34 -0500

    1.0.2.6

--- bt-pro-websites.php
+++ bt-pro-websites.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Buildertrend Pro Websites
 Description: Our required plugin for sharing common functionality across our client sites. 
-Version: 1.0.2.5
+Version: 1.0.2.6
 Author: John Stuifbergen
 License: GPLv2 or later
 Plugin URI:        https://github.com/buildertrend/bt-pro-websites
