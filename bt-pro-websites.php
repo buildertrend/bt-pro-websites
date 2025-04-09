4a208db0fe0ed4a6b8ae7d03b83323c2dcc69089
John Stuifbergen <johnstuif@gmail.com>
2025-04-09 14:41:06 -0500

    1.1.1 - fixes for check for updates

--- bt-pro-websites.php
+++ bt-pro-websites.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Buildertrend Pro Websites
 Description: Our required plugin for sharing common functionality across our client sites. 
-Version: 1.1.0
+Version: 1.1.1
 Author: BT Pro Websites Team
 License: GPLv2 or later
 Plugin URI:        https://github.com/buildertrend/bt-pro-websites
@@ -14,13 +14,11 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-require_once plugin_dir_path(__FILE__) . 'includes/admin-page.php';
+
 require_once plugin_dir_path(__FILE__) . 'includes/admin-dashboard-support-form.php';
 require_once plugin_dir_path(__FILE__) . 'includes/check-for-updates.php';
 require_once plugin_dir_path(__FILE__) . 'includes/enable-auto-updates.php';
-require_once plugin_dir_path(__FILE__) . 'includes/remove-dashboard-widgets.php';
-require_once plugin_dir_path(__FILE__) . 'includes/reset-dashboard-widgets.php';
-
 require_once plugin_dir_path(__FILE__) . 'includes/matterport.php';
 require_once plugin_dir_path(__FILE__) . 'includes/platmap.php';
-
+require_once plugin_dir_path(__FILE__) . 'includes/remove-dashboard-widgets.php';
+require_once plugin_dir_path(__FILE__) . 'includes/reset-dashboard-widgets.php';
--- includes/check-for-updates.php
+++ includes/check-for-updates.php
@@ -9,13 +9,13 @@ require plugin_dir_path(dirname(__FILE__)) . 'plugin-update-checker/plugin-updat
 use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
 
 $updateChecker = PucFactory::buildUpdateChecker(
-'https://github.com/buildertrend/bt-pro-websites',
-__FILE__,
-'bt-pro-websites'
+	'https://github.com/buildertrend/bt-pro-websites',
+	plugin_dir_path(dirname(__FILE__)) . 'bt-pro-websites.php',
+	'bt-pro-websites'
 );
 
 // Required for private repos:
 // $updateChecker->setAuthentication('your_github_personal_access_token');
 
-// Optional if you’re using a branch other than “master” (e.g., main)
+// Optional if you're using a branch other than "master" (e.g., main)
 $updateChecker->setBranch('main');
